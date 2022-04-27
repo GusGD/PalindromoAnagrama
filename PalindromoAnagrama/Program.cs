bd4f0b82b83cfb6dd7a9db0c4521e96103935ade
Gustavo <si_gustavo@outlook.com>
2022-04-27 12:56:07 -0300

    Retirando a ordenação das frases

--- PalindromoAnagrama/Program.cs
+++ PalindromoAnagrama/Program.cs
@@ -19,8 +19,8 @@ for (int x = i - 1; x >= 0; x--)
     fraseInverso = fraseInverso + frase;
 }
 
-fraseA = String.Concat(novaFrasePalindromo.OrderBy(a => a));
-fraseNA = String.Concat(fraseInverso.OrderBy(a => a));
+fraseA = String.Concat(novaFrasePalindromo);
+fraseNA = String.Concat(fraseInverso);
 
 //utilizei tabela da verdade para definir true ou false
 
